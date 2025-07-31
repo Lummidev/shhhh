67a50da2bab13717b51a129e4472318d09bf0ee2
Gabriel Valeriano <53843213+Lummidev@users.noreply.github.com>
2025-07-31 14:56:28 -0300

    feat: Add Ubuntu Sans font for usernames

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "@fontsource-variable/roboto";
+import "@fontsource-variable/ubuntu-sans";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
